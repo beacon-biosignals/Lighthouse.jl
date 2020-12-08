9023d48ac89cf5ebb5228e92ed22dca27eb8cca4
hannahilea <hannahilea@users.noreply.github.com>
2020-12-08 12:34:05 -0500

    Default docs deploy is now main not master

--- docs/make.jl
+++ docs/make.jl
@@ -5,4 +5,5 @@ makedocs(; modules=[Lighthouse], sitename="Lighthouse",
          authors="Beacon Biosignals and other contributors",
          pages=["API Documentation" => "index.md"])
 
-deploydocs(repo="github.com/beacon-biosignals/Lighthouse.jl.git")
+deploydocs(repo="github.com/beacon-biosignals/Lighthouse.jl.git",
+           latest="main")
