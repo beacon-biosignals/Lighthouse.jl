67f0e6322c8f4e8c71940929213225b69b815b05
SimonDanisch <sdanisch@gmail.com>
2021-05-20 11:24:53 +0200

    fail hard

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,10 @@ using Documenter
 makedocs(; modules=[Lighthouse], sitename="Lighthouse",
          authors="Beacon Biosignals and other contributors",
          pages=["API Documentation" => "index.md",
-                "Plotting" => "plotting.md"])
+                "Plotting" => "plotting.md"],
+         # makes docs fail hard if there is any error building the examples,
+         # so we don't just miss a build failure!
+         strict = true)
 
 deploydocs(repo="github.com/beacon-biosignals/Lighthouse.jl.git",
            devbranch="main", push_preview=true)
