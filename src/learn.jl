9a605864a771c0ce252aefa5a9e49613f755d0b1
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2022-05-03 16:29:26 +0200

    add logging interface (#60)
    * add logging interface
    
    * bump version
    
    * oops, add `log_value!`
    
    * Update src/learn.jl
    
    * add test
    
    * Apply suggestions from code review
    
    Co-authored-by: Hannah Robertson <hannahilea@users.noreply.github.com>
    
    * move LearnLoggers to separate file; implement default `log_event!`
    
    * update readme; fix ambiguity
    
    Co-authored-by: Hannah Robertson <hannahilea@users.noreply.github.com>

--- src/learn.jl
+++ src/learn.jl
@@ -1,55 +1,59 @@
 #####
-##### `LearnLogger`
+##### Logging interface
 #####
 
+# These must be implemented by every logger type.
+
 """
-    LearnLogger
+    log_plot!(logger, field::AbstractString, plot, plot_data)
 
-A struct that wraps a `TensorBoardLogger.TBLogger` in order to enforce the following:
+Log a `plot` to `logger` under field `field`.
 
-- all values logged to Tensorboard should be accessible to the `post_epoch_callback`
-  argument to [`learn!`](@ref)
-- all values that are cached during [`learn!`](@ref) should be logged to Tensorboard
+* `plot`: the plot itself
+* `plot_data`: an unstructured dictionary of values used in creating `plot`.
 
-To access values logged to a `LearnLogger` instance, inspect the instance's `logged` field.
+See also [`log_line_series!`](@ref).
 """
-struct LearnLogger
-    path::String
-    tensorboard_logger::TensorBoardLogger.TBLogger
-    logged::Dict{String,Vector{Any}}
-end
-
-function LearnLogger(path, run_name; kwargs...)
-    tensorboard_logger = TBLogger(joinpath(path, run_name); kwargs...)
-    return LearnLogger(path, tensorboard_logger, Dict{String,Any}())
-end
-
-function log_event!(logger::LearnLogger, value)
-    logged = string(now(), " | ", value)
-    TensorBoardLogger.log_text(logger.tensorboard_logger, "events", logged)
-    return logged
-end
-
-function log_plot!(logger::LearnLogger, field::AbstractString, plot, plot_data)
-    values = get!(() -> Any[], logger.logged, field)
-    push!(values, plot_data)
-    TensorBoardLogger.log_image(logger.tensorboard_logger, field, plot; step=length(values))
-    return plot
-end
-
-function log_value!(logger::LearnLogger, field::AbstractString, value)
-    values = get!(() -> Any[], logger.logged, field)
-    push!(values, value)
-    TensorBoardLogger.log_value(logger.tensorboard_logger, field, value;
-                                step=length(values))
-    return value
-end
+log_plot!(logger, field::AbstractString, plot, plot_data)
 
 
-function log_line_series!(logger::LearnLogger, field::AbstractString, series, series_labels)
-    @warn "`log_line_series!` not implemented for `LearnLogger`"
-    return nothing
+"""
+    log_value!(logger, field::AbstractString, value)
+
+Log a value `value` to `field`.
+"""
+log_value!(logger, field::AbstractString, value)
+
+
+"""
+    log_line_series!(logger, field::AbstractString, curves, labels=1:length(curves))
+
+Logs a series plot to `logger` under `field`, where...
+
+- `curves` is an iterable of the form `Tuple{Vector{Real},Vector{Real}}`, where each tuple contains `(x-values, y-values)`, as in the `Lighthouse.EvaluationRow` field `per_class_roc_curves`
+- `labels` is the class label for each curve, which defaults to the numeric index of each curve.
+"""
+log_line_series!(logger, field::AbstractString, curves, labels=1:length(curves))
+
+# The following have default implementations.
+
+"""
+    step_logger!(logger)
+
+Increments the `logger`'s `step`, if any. Defaults to doing nothing.
+"""
+step_logger!(::Any) = nothing
+
+
+"""
+    log_event!(logger, value::AbstractString)
+
+Logs a string event given by `value` to `logger`. Defaults to calling `log_value!` with a field named `event`.
+"""
+function log_event!(logger, value::AbstractString)
+    return log_value!(logger, "event", string(now(), " | ", value))
 end
+
 
 """
     log_evaluation_row!(logger, field::AbstractString, metrics)
@@ -83,49 +87,6 @@ function log_resource_info!(f, logger, section::AbstractString; suffix::Abstract
     return result
 end
 
-"""
-    Base.flush(logger)
-
-Persist possibly transient logger state.
-"""
-Base.flush(logger::LearnLogger) = nothing
-
-"""
-    forwarding_task = forward_logs(channel, logger::LearnLogger)
-
-Forwards logs with values supported by `TensorBoardLogger` to `logger::LearnLogger`:
-- string events of type `AbstractString`
-- scalars of type `Union{Real,Complex}`
-- plots that `TensorBoardLogger` can convert to raster images
-
-returns the `forwarding_task:::Task` that does the forwarding.
-To cleanly stop forwarding, `close(channel)` and `wait(forwarding_task)`.
-
-outbox is a Channel or RemoteChannel of Pair{String, Any}
-field names starting with "__plot__" forward to TensorBoardLogger.log_image
-"""
-function forward_logs(outbox, logger::LearnLogger)
-    @async try
-        while true
-            (field, value) = take!(outbox)
-            if typeof(value) <: AbstractString
-                log_event!(logger, value)
-            elseif startswith(field, "__plot__")
-                original_field = field[9:end]
-                values = get!(() -> Any[], logger.logged, original_field)
-                TensorBoardLogger.log_image(logger.tensorboard_logger, original_field,
-                                            value; step=length(values))
-            elseif typeof(value) <: Union{Real,Complex}
-                log_value!(logger, field, value)
-            end
-        end
-    catch e
-        if !(isa(e, InvalidStateException) && e.state == :closed)
-            @error "error forwarding logs, STOPPING FORWARDING!" exception = (e,
-                                                                              catch_backtrace())
-        end
-    end
-end
 
 #####
 ##### `predict!!`
@@ -186,7 +147,7 @@ end
     evaluate!(predicted_hard_labels::AbstractVector,
               predicted_soft_labels::AbstractMatrix,
               elected_hard_labels::AbstractVector,
-              classes, logger::LearnLogger;
+              classes, logger;
               logger_prefix, logger_suffix,
               votes::Union{Nothing,AbstractMatrix}=nothing,
               thresholds=0.0:0.01:1.0,
@@ -842,6 +803,7 @@ end
 #####
 
 """
+    upon(logger::LearnLogger, field::AbstractString; condition, initial)
     upon(logged::Dict{String,Any}, field::AbstractString; condition, initial)
 
 Return a closure that can be called to check the most recent state of
