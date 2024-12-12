7cfce070600acddbbff99f3019aa4a564efb2b46
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-12-12 16:29:27 +0100

    tests

--- test/utilities.jl
+++ test/utilities.jl
@@ -8,7 +8,8 @@ end
 
 @testset "`Lighthouse.area_under_curve`" begin
     @test_throws ArgumentError Lighthouse.area_under_curve([0, 1, 2], [0, 1])
-    @test ismissing(Lighthouse.area_under_curve([], []))
+    @test isnan(Lighthouse.area_under_curve([], []))
+    @test isnan(Lighthouse.area_under_curve([NaN], [NaN]))
     @test isapprox(Lighthouse.area_under_curve(collect(0:0.01:1), collect(0:0.01:1)), 0.5;
                    atol=0.01)
     @test isapprox(Lighthouse.area_under_curve(collect(0:0.01:(2π)), sin.(0:0.01:(2π))),
@@ -17,7 +18,8 @@ end
 
 @testset "`Lighthouse.area_under_curve_unit_square`" begin
     @test_throws ArgumentError Lighthouse.area_under_curve_unit_square([0, 1, 2], [0, 1])
-    @test ismissing(Lighthouse.area_under_curve_unit_square([], []))
+    @test isnan(Lighthouse.area_under_curve_unit_square([], []))
+    @test isnan(Lighthouse.area_under_curve_unit_square([NaN], [NaN]))
     @test isapprox(Lighthouse.area_under_curve_unit_square(collect(0:0.01:1),
                                                            collect(0:0.01:1)), 0.5;
                    atol=0.01)
