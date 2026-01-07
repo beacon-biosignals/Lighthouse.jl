907a3e07e78a5eb8c2f1c686129f2e07b3f665b0
Phillip Alday <phillip.alday@beacon.bio>
2026-01-07 08:52:11 -0600

    makie staleness

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 include("set_up_tests.jl")
 
 @testset "Aqua" begin
-    Aqua.test_all(Lighthouse; ambiguities=false)
+    # we ignore Makie because we do a manual install of a specific
+    # version to test specific versions
+    Aqua.test_all(Lighthouse;
+                  ambiguities=false, state_deps=(;ignore=:Makie))
 end
 
 include("plotting.jl")
